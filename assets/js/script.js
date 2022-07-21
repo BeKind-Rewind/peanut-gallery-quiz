c4687ac48d6e87586c13af0c2fae9f2261cab5c4
Amanda Perry <challenge641@gmail.com>
2022-07-20 19:28:28 -0500

    adding base files

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// code quiz
+
+// Create button so that WHEN I click the start button an action is performed
+
+
+// When I click THEN a timer starts 
+
+
+
+// When I click THEN I am presented with a question
+// Display is hidden on page load; toggles when button clicked
+
+
+
+// Create Questions with Answer arrays
+
+
+
+// WHEN I answer a question THEN I am presented with another question
+
+
+
+// WHEN I answer a question incorrectly THEN time is subtracted from the clock
+
+
+
+// END GAME_GAME OVER: WHEN all questions are answered or the timer reaches 0
+
+
+
+// WHEN the game is over THEN (prompt or show form) I can save my initials and score
+// PUSH to array in local storage
+
+
+
+
