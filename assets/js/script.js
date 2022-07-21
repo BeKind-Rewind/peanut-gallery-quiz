959c65f03153e189691160138f434f8e05e01583
Amanda Perry <challenge641@gmail.com>
2022-07-20 20:30:23 -0500

    noted questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // code quiz
 
+
 // Create button so that WHEN I click the start button an action is performed
 
 
@@ -30,6 +31,61 @@
 
 // WHEN the game is over THEN (prompt or show form) I can save my initials and score
 // PUSH to array in local storage
+
+
+// Q1 - Which of the following is the correct syntax to print a page using JavaScript?
+// A - window.print();*window.print(); is the correct option.
+// B - browser.print();
+// C - navigator.print();
+// D - document.print();
+
+// Q2 - Which of the following type of variable is visible only within a function where it is defined?
+// A - global variable
+// B - local variable *Local Variables: A local variable will be visible only within a function where it is defined. Function parameters are always local to that function.
+// C - Both of the above.
+// D - None of the above.
+
+// Q3 - Which built-in method returns the calling string value converted to lower case?
+// A - toLowerCase() *toLowerCase() method returns the calling string value converted to lower case.
+// B - toLower()
+// C - changeCase(case)
+// D - None of the above.
+
+// Q4 - Which of the following function of String object creates an HTML hypertext link that requests another URL?
+// A - link() *
+// B - sub()
+// C - sup()
+// D - small()
+
+// Q5 - Which of the following function of Array object removes the last element from an array and returns that element?
+// A - pop() *
+// B - push()
+// C - join()
+// D - map()
+
+// Q6 - Which of the following function of Array object returns a string representing the array and its elements?
+// A - toSource()
+// B - sort()
+// C - splice()
+// D - toString() *
+
+// Q7 Javascript is an _______ language?
+// Object-Oriented *
+// Object-Based
+// Procedural
+// None of the above
+
+// Q8 Which of the following methods is used to access HTML elements using Javascript?
+// getElementById()
+// getElementsByClass()
+// Both A & B *
+// None of the above
+
+
+
+
+
+
 
 
 
